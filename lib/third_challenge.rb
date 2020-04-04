1147f399b660e201fc541349c2f766ad67f39f5e
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 04:59:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,6 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name =>"Lord Montague",
+        :age =>53
         #your key/value pairs here
         },
       :matriarch => {
