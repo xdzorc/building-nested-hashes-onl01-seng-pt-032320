cf856b93d5e8b893bec516b51513c6576dd7dd95
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 05:01:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,9 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
+        :name =>"Romeo",
+        :age =>15,
+        :status =>"alive"
         #your key/value pairs here
         },
       :hero_friends => []
