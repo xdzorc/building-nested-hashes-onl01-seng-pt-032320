427f57c1a2a15988ca9512578cb3792fd777d459
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 05:03:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,9 +21,13 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
+        :name =>"Lord Capulet",
+        :age =>"51"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age =>"51"
         #your key/value pairs here
         },
       :heroine => {
