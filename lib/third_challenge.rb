e17fcc4053261a3ec86bb184104f083c1773b777
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 05:01:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,17 +3,17 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name =>"Lord Montague",
-        :age =>53
+        :age =>"53"
         #your key/value pairs here
         },
       :matriarch => {
         :name =>"Lady Montague",
-        :age =>54
+        :age =>"54"
         #your key/value pairs here
         },
       :hero => {
         :name =>"Romeo",
-        :age =>15,
+        :age =>"15",
         :status =>"alive"
         #your key/value pairs here
         },
