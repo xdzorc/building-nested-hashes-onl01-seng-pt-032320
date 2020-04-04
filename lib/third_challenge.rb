231573f2b856ef5f4f46990a74607842c68c8337
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 04:59:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,6 +7,8 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
+        :name =>"Lady Montague",
+        :age =>54
         #your key/value pairs here
         },
       :hero => {
