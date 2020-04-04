1d4ec2da99c1cbe437d071362438c2d8b948c529
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 04:53:34 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch =>{},
+      :matriarch =>{},
+      :hero =>{},
+      :hero_friends =>[]
+    }
       #your key/value pairs here!
       }, 
     :capulet => {
