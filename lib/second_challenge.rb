14182e973805f53f6434364c2d5f470804b802bd
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 04:56:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,10 +5,14 @@ def second_challenge
       :matriarch =>{},
       :hero =>{},
       :hero_friends =>[]
-    }
+     
       #your key/value pairs here!
       }, 
     :capulet => {
+      :patriarch =>{},
+      :matriarch =>{},
+      :heroine =>{},
+      :heroine_
       #your key/value pairs here!
       }}
 
